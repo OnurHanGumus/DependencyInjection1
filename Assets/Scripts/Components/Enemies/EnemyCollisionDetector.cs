d4d603e30253a34f75de73fc3251f62c82d0b99e
OnurHG <onurhangumus@gmail.com>
2022-12-31 16:33:53 +0300

    -Enemy pool fixed

--- Assets/Scripts/Components/Enemies/EnemyCollisionDetector.cs
+++ Assets/Scripts/Components/Enemies/EnemyCollisionDetector.cs
@@ -1,11 +1,14 @@
+using Events.External;
 using UnityEngine;
 using UnityEngine.Events;
+using Zenject;
 
 namespace Components.Enemies {
     public class EnemyCollisionDetector : MonoBehaviour, IAttackable
     {
         private int _enemyHits = 2;
-        
+        [Inject] private PoolSignals PoolSignals;
+        [SerializeField] private Enemy enemy;
         void IAttackable.OnWeaponTriggerEnter()
         {
             _enemyHits--;
@@ -13,6 +16,7 @@ namespace Components.Enemies {
             if (_enemyHits == 0)
             {
                 OnDeath?.Invoke(this);
+                PoolSignals.onRemoveEnemy?.Invoke(enemy);
             }
         }
 
